6c6df31ae18f1c7b984d131941518089cbdc7f85
W. Cayabyab <50767502+zhackdtech@users.noreply.github.com>
2025-02-17 10:56:57 +0800

    feat: Add background animation for theme switching
    Implemented a smooth background transition when switching between themes.
    - Added animation for background color change to enhance user experience.
    - Ensured the transition effect works seamlessly across theme changes.
    - Improved visual feedback when toggling between light and dark themes.
    
    This update improves the interactivity and aesthetics of theme switching.

--- src/layout/main/index.js
+++ src/layout/main/index.js
@@ -1,13 +1,27 @@
 import NavigationBar from "../../components/custom/navigationbar";
 import Footer from "../../components/custom/footer";
+import { Box } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import { useColorModeValue } from "../../components/ui/color-mode";
+
+const MotionBox = motion(Box);
 const MainLayout = (Component) => {
   const DefFunction = ({ ...props }) => {
+    const bodyBackground = useColorModeValue("#fafafa", "#000000");
     return (
-      <div>
+      <MotionBox
+        animate={{ backgroundColor: bodyBackground }} // Animate background color
+        transition={{ duration: 0.5, ease: "easeInOut" }} // Smooth transition
+        height={"100vh"}
+        width={"100vw"}
+        maxW={"100vw"}
+        maxH={"100vh"}
+        p={10}
+      >
         <NavigationBar />
         <Component {...props} />
         <Footer />
-      </div>
+      </MotionBox>
     );
   };
   return DefFunction;

4a6cb9647d25842c6e9d7ba46a0f8f958125412e
W. Cayabyab <50767502+zhackdtech@users.noreply.github.com>
2025-02-17 10:54:05 +0800

    feat: Add social media icon data array
    Created a data array for social media icons and links.
    - Includes icons for X, Facebook, and Instagram using react-icons.
    - Each entry contains the social media name, link, and corresponding icon.
    - Can be easily extended to add more social media platforms in the future.
    
    This structure improves scalability and maintainability of social media links.

---
+++ src/data/social/index.js
@@ -0,0 +1,20 @@
+import { FaX, FaFacebookF, FaInstagram } from "react-icons/fa6";
+const social = [
+  {
+    social: "x",
+    link: "#",
+    icon: <FaX />,
+  },
+  {
+    social: "facebook",
+    link: "#",
+    icon: <FaFacebookF />,
+  },
+  {
+    social: "instagram",
+    link: "#",
+    icon: <FaInstagram />,
+  },
+];
+
+export default social;

42d3d1e90160ee48e282c27ecb5254d46f50b3ec
Wilmarx John Cayabyab <50767502+zhackdtech@users.noreply.github.com>
2025-02-18 11:06:14 +0800

    chore: removed unused variables and imports

--- src/layout/main/index.js
+++ src/layout/main/index.js
@@ -1,23 +1,14 @@
 import NavigationBar from "../../components/custom/navigationbar";
-import { Box, Flex, Text } from "@chakra-ui/react";
-import { motion, AnimatePresence } from "framer-motion";
+import { Box, Flex } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 import { useColorModeValue } from "../../components/ui/color-mode";
 import SideNavigation from "../../components/custom/sidenav";
-import { useEffect, useState } from "react";
 import "../../design/main.scss";
 
 const MotionBox = motion(Box);
 
 const MainLayout = (Component) => {
   const DefFunction = ({ ...props }) => {
-    const [isLoading, setIsLoading] = useState(true);
-
-    useEffect(() => {
-      setTimeout(() => {
-        setIsLoading(false);
-      }, 5000);
-    }, []);
-
     const bodyBackground = useColorModeValue("#fafafa", "#000000");
 
     return (

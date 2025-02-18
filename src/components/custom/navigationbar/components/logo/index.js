c49f542790ed762e92c69a720ee4d88fec8aa19e
Wilmarx John Cayabyab <50767502+zhackdtech@users.noreply.github.com>
2025-02-18 15:27:24 +0800

    chore: hide brand when md screens and up

--- src/components/custom/navigationbar/components/logo/index.js
+++ src/components/custom/navigationbar/components/logo/index.js
@@ -7,6 +7,7 @@ const MotionBox = motion(Box);
 export default function Logo(props) {
   return (
     <MotionBox
+    display={{base:"none", smDown:"flex",md:"none",lg:"none"}}
       {...props}
       initial={{ opacity: 0, y: 20 }} // Start invisible and slightly lower
       animate={{ opacity: 1, y: 0 }} // Fade in and move up

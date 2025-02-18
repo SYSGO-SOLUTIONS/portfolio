18669cf8847b89364b23eb16d97e0f9e64ad69cc
Wilmarx John Cayabyab <50767502+zhackdtech@users.noreply.github.com>
2025-02-18 08:58:59 +0800

    chore: routes array

---
+++ src/routes.js
@@ -0,0 +1,24 @@
+const routes = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "About",
+    path: "/about",
+  },
+  {
+    name: "Projects",
+    path: "/projects",
+  },
+  //   {
+  //     name: "Contact",
+  //     path: "/contact",
+  //   },
+  {
+    name: "Testimonies",
+    path: "/testimonies",
+  },
+];
+
+export default routes;

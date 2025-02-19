8a23b613f1c23c7253585df0625e13bab11ee0c8
Wilmarx John Cayabyab <50767502+zhackdtech@users.noreply.github.com>
2025-02-19 13:08:07 +0800

    feat: added entrance and exit animations on pages

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,9 +1,27 @@
+import { motion, AnimatePresence } from "framer-motion";
+
 const Contact = () => {
-    return (
-        <div>
-            <h1>Contact</h1>
-        </div>
-    )
-}
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        key="main-content"
+        initial={{ opacity: 0, x: "100%" }} // Start off-screen (right)
+        animate={{
+          opacity: 1,
+          x: "0%",
+          transition: { duration: 0.7, ease: "easeInOut" }, // Entrance duration
+        }}
+        exit={{
+          opacity: 0,
+          x: "-100%",
+          transition: { duration: 0.7, ease: "easeInOut" }, // Exit duration
+        }}
+        style={{ width: "100%", height: "100%" }}
+      >
+        Contact
+      </motion.div>
+    </AnimatePresence>
+  );
+};
 
 export default Contact;
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,9 +1,27 @@
+import { motion, AnimatePresence } from "framer-motion";
+
 const Projects = () => {
-    return (
-        <div>
-            <h1>Projects</h1>
-        </div>
-    )
-}
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        key="main-content"
+        initial={{ opacity: 0, x: "100%" }} // Start off-screen (right)
+        animate={{
+          opacity: 1,
+          x: "0%",
+          transition: { duration: 0.7, ease: "easeInOut" }, // Entrance duration
+        }}
+        exit={{
+          opacity: 0,
+          x: "-100%",
+          transition: { duration: 0.7, ease: "easeInOut" }, // Exit duration
+        }}
+        style={{ width: "100%", height: "100%" }}
+      >
+        Projects
+      </motion.div>
+    </AnimatePresence>
+  );
+};
 
 export default Projects;

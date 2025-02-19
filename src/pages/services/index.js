f6e4325208bec47cc2e719b8b5584bf5091a0f2d
Wilmarx John Cayabyab <50767502+zhackdtech@users.noreply.github.com>
2025-02-19 13:07:41 +0800

    feat: services page

---
+++ src/pages/services/index.js
@@ -0,0 +1,27 @@
+import { motion, AnimatePresence } from "framer-motion";
+
+const Services = () => {
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        key="main-content"
+        initial={{ opacity: 0, x: "100%" }} // Start off-screen (right)
+        animate={{
+          opacity: 1,
+          x: "0%",
+          transition: { duration: 0.7, ease: "easeInOut" }, // Entrance duration
+        }}
+        exit={{
+          opacity: 0,
+          x: "-100%",
+          transition: { duration: 0.7, ease: "easeInOut" }, // Exit duration
+        }}
+        style={{ width: "100%", height: "100%" }}
+      >
+        Services
+      </motion.div>
+    </AnimatePresence>
+  );
+};
+
+export default Services;
